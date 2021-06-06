f6028066dc0a657efe424eb5e1457f7afe099b4d
JavierBlacksmith <34661253+JavierBlacksmith@users.noreply.github.com>
2021-06-05 22:36:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OMPCourse
+UE4
---
+++ Source/OMPCourse/OMPCourseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OMPCourseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOMPCourseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOMPCourseGameMode();
+};
+
+
+
